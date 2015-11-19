13408ec450eb1e1dee3dec15868922187b4a60cc
dgiac85 <dgiac85@gmail.com>
2015-11-19 19:48:12 +0100

    minor changes on Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function (grunt) {
             main: {
                 expand: true,
                 cwd: '',
-                src:['app/**','app/web-server.js','!app/lib/**','!app/modules/**/*.js','!app/js/**','!app/css/**'],
+                src:['app/**','app/web-server.js','!app/lib/**','!app/modules/**/*.js','!app/modules/**/*.css','!app/js/**','!app/css/**','!app/Prototypes/**/*','!app/Prototypes','!app/Prototypes/*'],
                 dest: 'dist/'
             }
         },
--- dist/app/Prototypes/End2EndTesting/testCtrl_spec.js
+++
@@ -1,8 +0,0 @@
-describe('Sample Hello App', function(){
-	it('should greet the named user',function(){
-		browser.get('http://localhost:8080/Prototypes/End2EndTesting/index.html');
-		element(by.model('firstName')).sendKeys('John');
-		var greeting=element(by.binding('firstName'));
-		expect(greeting.getText()).toEqual('Hello John!');
-	});
-});
